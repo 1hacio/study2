a6833dc9154f3de920fcd61ae2493c510ca2703c
Name <jyhspace@gmail.com>
2025-12-05 17:50:49 +0900

    아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -69,3 +69,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+    
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://api.dicebear.com/9.x/adventurer/svg?seed={self.author.username}'
